71bfde602585927d408d083f2e4b22e3ae1bc0fe
jsxrodman <jsxrodman@gmail.com>
2020-07-21 21:26:17 +0300

    Added fasta-execute function

--- src/alignment.py
+++ src/alignment.py
@@ -1,3 +1,24 @@
-seq1 = open('../data/CP000766.3.fasta')
-seq2 = open('../data/CP003305.1.fasta')
+from datetime import datetime
+from utils import execute_fasta
 
+# Do not rename or delete
+# This's used in alignment functions
+seq1, seq2 = None, None
+
+data = execute_fasta('../data/task1_sequences.fasta')
+
+r = datetime.now()
+
+# Re-definition
+seq1, seq2 = data[0]['val'], data[1]['val']
+
+print(seq1[:120])
+print(seq2[:120])
+
+test = execute_fasta('../data/CP000766.3.fasta')[0]['val']
+
+i = 0
+while i < len(test):
+    i += 1
+print(len(test))
+print(datetime.now() - r)
