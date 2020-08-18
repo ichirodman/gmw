b5d3b3b07266a83f8fb9ce555dfb922aa39ce2fe
jsxrodman <jsxrodman@gmail.com>
2020-08-18 14:55:30 +0300

    Added main dir

--- py/tests/simple_dust_test.py
+++ py/tests/simple_dust_test.py
@@ -1,15 +1,17 @@
 from py.algos.dust import dust
 from py.utils.fasta import extract_fasta
 
-seq = extract_fasta('CP003305.1.fasta')[0]['data']
-# seq = 'AAAACATATTTTTTTGAGTGGGAATTTTGTAAAAAAAATTCATTCAAACCCCCCCCCCCTCCCCCTCGGTTATGTTATTAT'
+# seq = extract_fasta('CP003305.1.fasta')[0]['data']
+seq = 'AAAACATATTTTTTTGTTTTTTTTTAGTGGGAATTTTGTAAAAAAAATTCATTCAAACCCCCCCCCCCTCCCCCTCGGTTATGTTATTAT'
 dusted, cut = dust(seq, n_chars=True)
-'''print('\t'.join(dusted))
+
+print('\t'.join(dusted))
 print('\t'.join(seq))
 print('\t'.join([str(i) for i in range(len(seq))]))
 print()
 print(seq)
-print(dusted)'''
+print(dusted)
 
+exit()
 for k in cut:
     print('\n{}\n{}'.format(seq[k[0] - 3:k[1] + 3], '-' * 3 + dusted[k[0]: k[1]] + '-' * 3))
