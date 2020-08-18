fb025bd6114d6b163cc5c621d9bb719948fdd779
jsxrodman <jsxrodman@gmail.com>
2020-08-18 21:23:43 +0300

    Fixed dust algo

--- py/algos/dust.py
+++ py/algos/dust.py
@@ -1,49 +1,57 @@
-def dust(seq: str, min_len: int = 7, min_score=2.5, threshold_score_difference=0.7, n_chars=False,
+def dust(seq: str, min_len: int = 7, score_increase_threshold=2.5, threshold_score_difference=0.7, n_chars=False,
          print_progress=False):
-    _to_cut = list()
-    _l = min_len - 2
-    _amount = [0] * (4 ** 3)
-    _i = 0
-    seq_len = len(seq)
-    done = 0
-    while _i + 3 < seq_len:
-        if print_progress and round((_i + 3) * 100. / seq_len) > done:
-            done = int(round((_i + 1) * 100. / (seq_len - 3)))
-            print('done : {}%'.format(done))
-        _amount[_get_slice_index(seq[_i:_i + 3])] += 1
-        if _i > _l:
-            _amount[_get_slice_index(seq[_i - _l:_i - _l + 3])] -= 1
-            score = _count_dust_score(_amount, _l)
-            if score > min_score:
-                _add_index = 1
-                _add_amount = [0] * (4 ** 3)
-                _score_to_compare_with = score
-                _new_score = None
+    __slice_len = 3
+    __to_filter_out, __amount_array = list(), [0] * (4 ** __slice_len)
+    __i, __slices_amount = 0, min_len - 2
+    __seq_len = len(seq)
+    __done = 0
+
+    while __i + __slice_len < __seq_len:
+        if print_progress and round((__i + __slice_len) * 100. / __seq_len) > __done:
+            __done = int(round((__i + 1) * 100. / (__seq_len - __slice_len)))
+            # print('__done : {}%'.format(__done))
+
+        __amount_array[_get_slice_index(seq[__i:__i + __slice_len])] += 1
+        
+        if __i > __slices_amount:
+            __amount_array[_get_slice_index(seq[__i - __slices_amount:__i - __slices_amount + __slice_len])] -= 1
+            __sub_seq_score = _count_dust_score(__amount_array, __slices_amount)
+            
+            if __sub_seq_score > score_increase_threshold:
+                __additive_i = 1
+                __additive_amount_array = [0] * (4 ** __slice_len)
+                __score_to_compare_with = __sub_seq_score
+                __new_score = list()
                 while True:
-                    _add_amount[_get_slice_index(seq[_i + _add_index:_i + 3 + _add_index])] += 1
-                    _new_score = _count_dust_score(_amount, _l, additive_array=_add_amount)
-                    # print(_new_score - _score_to_compare_with)
-                    if _new_score - _score_to_compare_with < threshold_score_difference:
-                        break
-                    _score_to_compare_with = _new_score
-                    _add_index += 1
-                _add_amount[_get_slice_index(seq[_i + _add_index:_i + 3 + _add_index])] -= 1
-                _to_cut.append([_i - _l + 1 + 1, _i + 3 + _add_index - 1, _score_to_compare_with])
-                print_progress and print("add: {}".format(str(_to_cut[-1])))
-                print_progress and print("{} : {} || {}".format(seq[_i - _l + 1: _i + 3], score, _new_score))
-                _i = _to_cut[-1][1]
-                _amount = [0] * (4 ** 3)
-        _i += 1
-    print_progress and print(_to_cut)
-    for segment in _to_cut:
+                    __additive_amount_array[_get_slice_index(seq[__i + __additive_i:__i + __slice_len + __additive_i])] += 1
+                    __new_score.append(_count_dust_score(__amount_array, __slices_amount, additive_array=__additive_amount_array))
+                    print_progress and print(
+                        'scores difference: {}'.format(__new_score[-1] - __score_to_compare_with))
+                    if len(__new_score) > 3:
+                        if __new_score[-1] - __score_to_compare_with < threshold_score_difference:
+                            break
+                        __score_to_compare_with = __new_score[-1]
+                    __additive_i += 1
+                __additive_amount_array[_get_slice_index(seq[__i + __additive_i:__i + __slice_len + __additive_i])] -= 1
+                __to_filter_out.append([__i - __slices_amount + 1 + 1, __i + __slice_len + __additive_i - 1, __score_to_compare_with])
+                print_progress and print("add: {}".format(str(__to_filter_out[-1])))
+                print_progress and print("{} : {} || {}".format(seq[__i - __slices_amount + 1: __i + __slice_len], __sub_seq_score, __new_score))
+                __i = __to_filter_out[-1][1]
+                __amount_array = [0] * (4 ** __slice_len)
+                
+        __i += 1
+        
+    print_progress and print(__to_filter_out)
+    for segment in __to_filter_out:
         seq = seq[:segment[0]] + (
             seq[segment[0]:segment[1]].lower() if not n_chars else 'N' * (segment[1] - segment[0])
         ) + seq[segment[1]:]
-    return seq, _to_cut
+    return seq, __to_filter_out
 
 
 def _get_slice_index(seq_slice: str) -> int:
-    return sum(int(i) * j for i, j in zip(''.join([str(_get_nucleotide_number(i)) for i in seq_slice]), [16, 4, 1]))
+    return sum(int(i) * j for i, j in zip(''.join([str(_get_nucleotide_number(i)) for i in seq_slice]),
+                                          [2 ** i for i in range(len(seq_slice))]))
 
 
 def _get_nucleotide_number(nucleotide: str) -> int:
@@ -59,9 +67,9 @@ def _get_nucleotide_number(nucleotide: str) -> int:
         raise "Something strange: {}".format(nucleotide)
 
 
-def _count_dust_score(occurrence_array: list, l_value: int, additive_array: list = None) -> int:
+def _count_dust_score(occurrence_array: list, __slices_amount: int, additive_array: list = None) -> int:
     if not additive_array:
-        return sum([(i * (i + 1)) / 2. for i in occurrence_array if i != 0]) / float(l_value - 1)
+        return sum([(i * (i + 1)) / 2. for i in occurrence_array if i != 0]) / float(__slices_amount - 1)
     else:
-        return sum([((i + j) * (i + 1 + j)) / 2. for i, j in zip(occurrence_array, additive_array)]) / float(
-            l_value - 1)
+        return sum([((i + j) * (i + 1 + j)) / 2. for i, j in zip(occurrence_array, additive_array)]) \
+               / float(__slices_amount - 1)
