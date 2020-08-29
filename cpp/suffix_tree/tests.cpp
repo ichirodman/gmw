42e6af87eb1c073a75166b4a289693456fbec0c0
jsxrodman <jsxrodman@gmail.com>
2020-08-29 12:12:37 +0300

    Prettified suffix tree tests

--- cpp/suffix_tree/tests.cpp
+++ cpp/suffix_tree/tests.cpp
@@ -8,51 +8,94 @@ using namespace std;
 #define TLF ">> " // Test left framing
 #define TRF " <<" // Test right framing
 
-void test_suffix_tree_constructor();
+bool test_suffix_tree_constructor();
 
-void test_suffix_tree_string_slice_getter();
+bool test_suffix_tree_string_slice_getter();
 
-void test_suffix_tree_prefix_restoring();
+bool test_suffix_tree_prefix_restoring();
+
+bool test_suffix_tree_vertex_setting_info();
 
 int main()
 {
-    // test_suffix_tree_constructor();
-    // test_suffix_tree_string_slice_getter();
-    test_suffix_tree_prefix_restoring();
+    if (!test_suffix_tree_constructor())
+        return 1;
+    if (!test_suffix_tree_string_slice_getter())
+        return 2;
+    if (!test_suffix_tree_prefix_restoring())
+        return 3;
+    if (!test_suffix_tree_vertex_setting_info())
+        return 4;
     return 0;
 }
 
-void test_suffix_tree_constructor()
+bool test_suffix_tree_constructor()
 {
     cout << TF << "Started test suffix tree constructor" << TF << endl;
+
     string const ss("flower");
     SuffixTree *st = new SuffixTree(ss);
-    cout << TLF << "Tree string: " << st->get_string_tree_slice(0, ss.length()) << "                 ; Expected: " << ss << TRF << endl;
-    cout << TLF << "Root vertex address: " << st->get_root() << " ; Expected: any address" << TRF << endl;
+    string tree_string = st->get_string_tree_slice(0, ss.length());
+    SuffixTreeVertex *tree_root_vertex_address = st->get_root();
+
+    cout << TLF << "Tree string: " << tree_string << "                 ; Expected: " << ss << TRF << endl;
+    cout << TLF << "Root vertex address: " << tree_root_vertex_address << " ; Expected: any address" << TRF << endl;
+
     delete st;
-    cout << TF << "Finished test" << TF << endl;
+    cout << TF << "Finished test" << TF << endl
+         << endl;
+    return tree_string == ss && tree_root_vertex_address != nullptr;
 }
 
-void test_suffix_tree_string_slice_getter()
+bool test_suffix_tree_string_slice_getter()
 {
     cout << TF << "Started test suffix tree constructor" << TF << endl;
+
     string const ss("flower");
     SuffixTree *st = new SuffixTree(ss);
     size_t i = 2, length = 4;
-    cout << TLF << "Tree string slice: " << st->get_string_tree_slice(i, length) << " ; Expected: " << ss.substr(i, length) << TRF << endl;
+    string tree_string_slice = st->get_string_tree_slice(i, length), expected_slice = ss.substr(i, length);
+
+    cout << TLF << "Tree string slice: " << tree_string_slice << " ; Expected: " << expected_slice << TRF << endl;
+
     delete st;
-    cout << TF << "Finished test" << TF << endl;
+    cout << TF << "Finished test" << TF << endl
+         << endl;
+    return tree_string_slice == expected_slice;
 }
 
-void test_suffix_tree_prefix_restoring()
+bool test_suffix_tree_prefix_restoring()
 {
     cout << TF << "Started test suffix tree prefix restoring" << TF << endl;
+
     string const ss("prefix");
     SuffixTree *st = new SuffixTree(ss);
     SuffixTreeVertex *stv1 = new SuffixTreeVertex(0, 2, st->get_root());
     SuffixTreeVertex *stv2 = new SuffixTreeVertex(2, 2, stv1);
     SuffixTreeVertex *stv3 = new SuffixTreeVertex(4, 2, stv2);
-    cout << TLF << "Tree prefix restoring: " << st->restore_prefix(stv3) << " ; Expected: " << ss << TRF << endl;
+    string restored_prefix = st->restore_prefix(stv3);
+
+    cout << TLF << "Tree prefix restoring: " << restored_prefix << " ; Expected: " << ss << TRF << endl;
+
     delete st;
-    cout << TF << "Finished test" << TF << endl;
+    cout << TF << "Finished test" << TF << endl
+         << endl;
+    return restored_prefix == ss;
+}
+
+bool test_suffix_tree_vertex_setting_info()
+{
+    cout << TF << "Started test suffix tree prefix restoring" << TF << endl;
+
+    int entry = 2, length = 5;
+    SuffixTreeVertex *stv = new SuffixTreeVertex(0, 0);
+    stv->set_vertex_info(entry, length);
+    int stv_entry_index = stv->get_entry_index(), stv_substring_length = stv->get_length();
+
+    cout << TLF << "Tree vertex basic info: " << stv_entry_index << ", " << stv_substring_length << " ; Expected: " << entry << ", " << length << endl;
+
+    delete stv;
+    cout << TF << "Finished test" << TF << endl
+         << endl;
+    return stv_entry_index == entry && stv_substring_length == length;
 }
