f892c8e80fdc39699e2c7183683e3064480b0f7e
ichirodman <jsxrodman@gmail.com>
2020-09-29 23:13:01 +0300

    Refactored main for presenting a simple string tree

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,10 +8,11 @@ int main() {
     std::string filename = "large1/large_genome1.fasta";
     FastaFileContent * ffc = new FastaFileContent(filename);
     std::string sequence = ffc->sequences.at(0)->source->substr(0, 125000);
-    SuffixTreeBuilder * builder = new SuffixTreeBuilder(sequence);
-    startTiming();
+    // SuffixTreeBuilder * builder = new SuffixTreeBuilder(sequence);
+    SuffixTreeBuilder * builder = new SuffixTreeBuilder("abbacabaa");
+    // startTiming();
     builder->build();
-    finishTiming();
-    // builder->printDebugView();
+    // finishTiming();
+    builder->printDebugView();
     return 0;
 }
